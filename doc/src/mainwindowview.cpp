391334e1865deff988f9c907d2ebe1483ea5e367
juliagoda <juliagoda.pl@protonmail.com>
2022-07-13 20:47:45 +0200

    Add KF5

---
+++ doc/src/mainwindowview.cpp
@@ -0,0 +1,46 @@
+/*
+    SPDX-FileCopyrightText: %{CURRENT_YEAR} %{AUTHOR} <%{EMAIL}>
+
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+*/
+
+// application headers
+#include "mainwindowview.h"
+
+#include "pakGuiSettings.h"
+
+#include <KLazyLocalizedString>
+
+
+MainWindowView::MainWindowView(QWidget *parent)
+    : QWidget(parent)
+{
+    m_ui.setupUi(this);
+    handleSettingsChanged();
+}
+
+MainWindowView::~MainWindowView()
+{
+}
+
+void MainWindowView::switchColors()
+{
+    // switch the foreground/background colors of the label
+    QColor color = pakGuiSettings::colorBackground();
+    pakGuiSettings::setColorBackground(pakGuiSettings::colorForeground());
+    pakGuiSettings::setColorForeground(color);
+
+    handleSettingsChanged();
+}
+
+void MainWindowView::handleSettingsChanged()
+{
+    QPalette palette = m_ui.templateLabel->palette();
+    palette.setColor(QPalette::Window, pakGuiSettings::colorBackground());
+    palette.setColor(QPalette::WindowText, pakGuiSettings::colorForeground());
+    m_ui.templateLabel->setPalette(palette);
+
+    // i18n : internationalization
+    m_ui.templateLabel->setText(i18n("This project is %1 days old", pakGuiSettings::ageInDays()));
+}
+
