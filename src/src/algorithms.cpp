d75c0625a65605bc8a92746d039f272bb1650825
juliagoda <juliagoda.pl@protonmail.com>
2023-05-11 18:36:20 +0200

    refactor - algorithms

--- src/src/algorithms.cpp
+++ src/src/algorithms.cpp
@@ -4,7 +4,7 @@
 
 template<typename T>
 QStringList Algorithms::createSplittedList(const QString& text,
-                                           const QString& splitting_text,
+                                           const QString& separator,
                                            const QList<T>& list,
                                            const QMap<int, T>& map)
 {
@@ -22,40 +22,103 @@ QStringList Algorithms::createSplittedList(const QString& text,
     if (text.isEmpty() || selected_infos.isEmpty())
         return QStringList();
 
+    createListOfLines<T>(selected_infos, text, separator, list, map);
+    return selected_infos;
+}
+
+
+template<typename T>
+void Algorithms::createListOfLines(QStringList& selected_infos,
+                                   const QString& text,
+                                   const QString& separator,
+                                   const QList<T>& list,
+                                   const QMap<int, T>& map)
+{
     auto lines_list = text.split('\n');
     int i = 0;
     int last_index = -1;
-    int index_of_double_colon = 0;
+    int index_of_separator = 0;
 
     for (auto line_it = lines_list.begin(); line_it != lines_list.end(); line_it++)
     {
-        if ((*line_it).contains(splitting_text))
-        {
-            i++;
-            index_of_double_colon = (*line_it).indexOf(splitting_text) + splitting_text.count();
-            auto tooltip_part = map.value(i);
-            if (list.contains(tooltip_part))
-            {
-                selected_infos[list.indexOf(tooltip_part)] = *line_it;
-                last_index = list.indexOf(tooltip_part);
-            }
-            else
-            {
-                last_index = -1;
-            }
-
+        if (foundIndexOdSeparator<T>(line_it, separator, index_of_separator, list, map, i, selected_infos, last_index))
             continue;
-        }
 
         if (last_index < 0)
             continue;
 
         QString leading_whitespaces{};
-        selected_infos[last_index] += "\n" + leading_whitespaces.fill(' ', index_of_double_colon) + *line_it;
+        selected_infos[last_index] += "\n" + leading_whitespaces.fill(' ', index_of_separator) + *line_it;
     }
-
-    return selected_infos;
 }
 
 
-template QStringList Algorithms::createSplittedList(const QString& text, const QString& splitting_text, const QList<Package::TooltipLine>& list, const QMap<int, Package::TooltipLine>& map);
+template<typename T>
+bool Algorithms::foundIndexOdSeparator(const QStringList::iterator& line_it,
+                                       const QString& separator,
+                                       int& index_of_separator,
+                                       const QList<T>& list,
+                                       const QMap<int, T>& map,
+                                       int& i,
+                                       QStringList& selected_infos,
+                                       int& last_index)
+{
+    if ((*line_it).contains(separator))
+    {
+        i++;
+        index_of_separator = (*line_it).indexOf(separator) + separator.count();
+        auto tooltip_part = map.value(i);
+        last_index = getFirstIndexAfterSeparator<T>(list, tooltip_part, selected_infos, line_it);
+        return true;
+    }
+
+    return false;
+}
+
+
+
+template<typename T>
+int Algorithms::getFirstIndexAfterSeparator(const QList<T>& list,
+                                            T tooltip_part,
+                                            QStringList& selected_infos,
+                                            const QStringList::iterator& line_it)
+{
+    if (list.contains(tooltip_part))
+    {
+        selected_infos[list.indexOf(tooltip_part)] = *line_it;
+        return list.indexOf(tooltip_part);
+    }
+
+    return -1;
+}
+
+
+// forward declarations
+
+template QStringList Algorithms::createSplittedList(const QString& text,
+                                                    const QString& separator,
+                                                    const QList<Package::TooltipLine>& list,
+                                                    const QMap<int, Package::TooltipLine>& map);
+
+template void Algorithms::createListOfLines(QStringList& selected_infos,
+                                            const QString& text,
+                                            const QString& separator,
+                                            const QList<Package::TooltipLine>& list,
+                                            const QMap<int, Package::TooltipLine>& map);
+
+template bool Algorithms::foundIndexOdSeparator(const QStringList::iterator& line_it,
+                                                const QString& separator,
+                                                int& index_of_separator,
+                                                const QList<Package::TooltipLine>& list,
+                                                const QMap<int, Package::TooltipLine>& map,
+                                                int& i,
+                                                QStringList& selected_infos,
+                                                int& last_index);
+
+template int Algorithms::getFirstIndexAfterSeparator(const QList<Package::TooltipLine>& list,
+                                                     Package::TooltipLine tooltip_part,
+                                                     QStringList& selected_infos,
+                                                     const QStringList::iterator& line_it);
+
+
+
--- src/src/algorithms.h
+++ src/src/algorithms.h
@@ -9,8 +9,33 @@ class Algorithms
 public:
     template<typename T>
     QStringList createSplittedList(const QString& text,
-                                   const QString& splitting_text,
+                                   const QString& separator,
                                    const QList<T>& list,
                                    const QMap<int, T>& map);
+
+private:
+    template<typename T>
+    void createListOfLines(QStringList& selected_infos,
+                           const QString& text,
+                           const QString& separator,
+                           const QList<T>& list,
+                           const QMap<int, T>& map);
+
+
+    template<typename T>
+    bool foundIndexOdSeparator(const QStringList::iterator& line_it,
+                               const QString& separator,
+                               int& index_of_separator,
+                               const QList<T>& list,
+                               const QMap<int, T>& map,
+                               int& i,
+                               QStringList& selected_infos,
+                               int& last_index);
+
+    template<typename T>
+    int getFirstIndexAfterSeparator(const QList<T>& list,
+                                    T tooltip_part,
+                                    QStringList& selected_infos,
+                                    const QStringList::iterator& line_it);
 };
 
